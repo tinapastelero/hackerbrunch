d859f7cc10dace16b72636bb86b324800a594837
Tina Pastelero <tinapastelero@gmail.com>
2016-09-15 13:57:58 -0700

    debug false

--- server.py
+++ server.py
@@ -101,7 +101,7 @@ def resto_markers():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     connect_to_db(app, os.environ.get("DATABASE_URL"))
     # Use the DebugToolbar
     DebugToolbarExtension(app)
