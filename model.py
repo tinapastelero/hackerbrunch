ecf8829328dc713d437c27e9797c131acb486aeb
Tina Pastelero <tinapastelero@gmail.com>
2016-09-15 14:25:00 -0700

    model file db link

--- model.py
+++ model.py
@@ -123,11 +123,11 @@ class Yelp_Detail(db.Model):
 ##############################################################################
 # Helper functions
 
-def connect_to_db(app, db_uri="DATABASE_URL"):
+def connect_to_db(app, db_uri=None):
     """Connect the database to our Flask app."""
 
     # Configure to use our PstgreSQL database
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri or 'postgres:///restaurants'
     db.app = app
     db.init_app(app)
 
