1f9a24ef017535dcee4890dfaf63ab04bf12fbf6
Tina Pastelero <tinapastelero@gmail.com>
2016-09-15 14:25:26 -0700

    model file db link

--- model.py
+++ model.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 db = SQLAlchemy()
-DATABASE_URL = os.environ("DATABASE_URL")
 
 ##############################################################################
 # Model definitions
